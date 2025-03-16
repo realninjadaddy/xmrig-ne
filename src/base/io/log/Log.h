652615ea705af26b0367f9d666db471e049e4b83
Ninja <ninja@ninjabuilder.ninjahouse.internal>
2025-03-16 19:58:44 +0100

    copyright notices

--- src/base/io/log/Log.h
+++ src/base/io/log/Log.h
@@ -2,6 +2,7 @@
  * Copyright (c) 2019      Spudz76     <https://github.com/Spudz76>
  * Copyright (c) 2018-2021 SChernykh   <https://github.com/SChernykh>
  * Copyright (c) 2016-2021 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright (c) 2025-2025 NinjaDaddy  <https://github.com/realninjadaddy/xmrig-ne>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
--- src/core/config/usage.h
+++ src/core/config/usage.h
@@ -6,6 +6,7 @@
  * Copyright (c) 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright (c) 2018-2024 SChernykh   <https://github.com/SChernykh>
  * Copyright (c) 2016-2024 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright (c) 2025-2025 NinjaDaddy  <https://github.com/realninjadaddy/xmrig-ne>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
